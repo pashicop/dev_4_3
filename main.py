701260206b5fdfbd9b5cc9b3d466297b51f97dc4
Pavel <97126500+pashicop@users.noreply.github.com>
2022-03-17 01:44:49 +0300

    get_ips

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import socket
 
+def get_one_ip():
+    serv_ip = {}
+    for service in services:
+        serv_ip[service] = socket.gethostbyname(service)
+    # print(serv_ip)
+    return serv_ip
+
+def get_all_ip():
+    serv_ip = {}
+    for service in services:
+        serv_ip[service] = socket.gethostbyname_ex(service)[2]
+    # print(serv_ip)
+    return serv_ip
 if __name__ == '__main__':
     services = ['drive.google.com', 'mail.google.com', 'google.com']
-    serv_ip = {}
-    # print(type(socket.gethostbyname_ex('mail.google.com')))
-
-    # print(ips)
-    for service in services:
-        serv_ip[service] = socket.gethostbyname_ex('mail.google.com')[2]
-    print(serv_ip)
+    list_ip = get_one_ip()
+    list_ips = get_all_ip()
+    print(list_ip)
+    print(list_ips)
