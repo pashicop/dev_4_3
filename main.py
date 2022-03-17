a2705471411590cf4bbc5b4813e91a2d14e30b9d
Pavel Shitikov <pashicop@gmail.com>
2022-03-17 16:18:28 +0300

    Task 2 comment print

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import socket
 import time
 
+
 def get_one_ip():
     serv_ip = {}
     for service in services:
         serv_ip[service] = socket.gethostbyname(service)
     return serv_ip
+
 
 def get_all_ip():
     serv_ip = {}
@@ -13,19 +15,17 @@ def get_all_ip():
         serv_ip[service] = socket.gethostbyname_ex(service)[2]
     return serv_ip
 
-# def get_new_ip(service):
-#     ip = socket.gethostbyname(service)
-#     return ip
 
-def print_log(list_ip):
-    for service, ip in list_ip.items():
+def print_log(l_ip):
+    for service, ip in l_ip.items():
         ip_new = socket.gethostbyname(service)
         if ip != ip_new:
             print(f'[ERROR] {service} IP mismatch: {ip} {ip_new}')
-            list_ip[service] = ip_new
+            l_ip[service] = ip_new
             # print(list_ip)
         print(f'{service} - {ip_new}')
     time.sleep(1)
+
 
 if __name__ == '__main__':
     services = ['drive.google.com', 'mail.google.com', 'google.com']
