818d422cffb2168f1ac18d73ae5b272f39f7edf0
Pavel <pashicop@gmail.com>
2022-04-06 11:00:21 +0300

    add ping from db

--- main.py
+++ main.py
@@ -54,10 +54,10 @@ def connect_db():
             password=cred["password"],
             database="test"
         )
-        print(connection)
-        print(f'Database {test_db} Connected!')
-    except mysql.connector.Error as e:
-        print(e)
+        # print(connection)
+        print(f'БД {test_db} подключена!')
+    except mysql.connector.Error as err:
+        print(err)
     # time.sleep(120)
     if connection:
         return connection
@@ -68,7 +68,7 @@ def connect_db():
 def close_db(con_db):
     # time.sleep(20)
     con_db.close()
-    print("Database connection closed")
+    print("БД отключена")
 
 
 def get_one_ip(in_services):
@@ -87,104 +87,120 @@ def get_all_ip():
     return serv_ip
 
 
-def save_ip(list_ip: list):
+def save_init_ip(serv_ip: list, con_db):
     with open("output/1.json", "w") as js:
-        json.dump(list_ip, js)
-        json.dump(str(datetime.now()), js)
+        list_out = serv_ip.copy()
+        list_out.insert(0, {"date": str(datetime.now())})
+        json.dump(list_out, js)
+        # json.dump(str(datetime.now()), js)
     with open("output/1.yaml", "w") as ym:
-        yaml.dump(list_ip, ym, indent=2, explicit_start=True, explicit_end=True)
-        yaml.dump(str(datetime.now()), ym, explicit_start=True, explicit_end=True)
-    with open("credentials.json", encoding="UTF-8") as f:
-        dict = json.load(f)
+        yaml.dump(list_out, ym, indent=2, explicit_start=True, explicit_end=True)
+        # yaml.dump(str(datetime.now()), ym, explicit_start=True, explicit_end=True)
     try:
-        with mysql.connector.connect(
-                host="localhost",
-                user=dict["user"],
-                password=dict["password"],
-                database="test"
-        ) as connection:
-            print(connection)
-            known_dns = []
-            db_query = """
-                SELECT 
-                    ipt.id, 
-                    dns.dns,
-                    ipt.cur_ip_id, 
-                    ip1.ip AS current_ip, 
-                    ipt.old_ip_id, 
-                    ip2.ip AS old_ip 
-                FROM ip_track ipt 
-                LEFT JOIN dns
-                    ON dns.id = ipt.dns_id
-                LEFT JOIN ip ip1
-                    ON ip1.id = ipt.cur_ip_id 
-                LEFT JOIN ip ip2
-                    ON ip2.id = ipt.old_ip_id
-                """
-            with connection.cursor() as cursor:
-                cursor.execute(db_query)
-                result = cursor.fetchall()
-                for row in result:
-                    print(row)
-                    if row[1]:
-                        known_dns.append(row[1])
-            for service in list_ip:
-                for service_name, service_ip in service.items():
-                    if service_name in known_dns:
-                        db_query_select_id_from_dns = "SELECT id FROM test.dns WHERE dns = '" + service_name + "'"
-                        print(db_query_select_id_from_dns)
-                        with connection.cursor() as cursor2:
-                            cursor2.execute(db_query_select_id_from_dns)
-                            id_from_dns = int(cursor2.fetchone()[0])
-                            print(id_from_dns)
-                    with connection.cursor() as cursor3:
-                        db_query_select_ip_id_from_ip = "SELECT id FROM test.ip WHERE ip = '" + service_ip + "'"
-                        print(db_query_select_ip_id_from_ip)
-                        cursor3.execute(db_query_select_ip_id_from_ip)
-                        result3 = cursor3.fetchone()
-                        if result3:
-                            id_ip_from_ip = int(result3[0])
-                        else:
-                            id_ip_from_ip = 0
-                    if id_ip_from_ip == 0:
-                        with connection.cursor() as cursor4:
-                            db_query_insert = "INSERT INTO ip(ip) VALUES ('" + service_ip + "')"
-                            print(db_query_insert)
-                            cursor4.execute(db_query_insert)
-                            connection.commit()
-                            db_query_ips = "SELECT id FROM ip WHERE ip = '" + service_ip + "'"
-                            print(db_query_ips)
-                            cursor4.execute(db_query_ips)
-                            id_ip_from_ip = int(cursor4.fetchone()[0])
-                    print(id_ip_from_ip)
-                    # id_ip_from_ip = "NULL"
-                    print()
-
-                    print()
-                    # with connection.cursor() as cursor4:
-
-                    # known_ip.append(row[1])
-                # if ip not in known_ip:
-                #     cursor.execute(db_query)
-                #     connection.commit()
-            # for serv_ip_dict in list_ip:
-            #     for serv, ip in serv_ip_dict.items():
-            #         db_query = "INSERT INTO ip_track(dns_id, cur_ip_id, old_ip_id) VALUES ('" + serv_id + "', '" + cur_ip_id + "'" + old_ip_id + "')"
-            #         db_query2 = "SELECT * FROM ip"
-            # for ip in list_ip2:
-            #     known_ip = []
-            #     db_query = "INSERT INTO ip(ip) VALUES ('" + ip + "')"
-            #     db_query2 = "SELECT * FROM ip"
-            #     print(db_query)
-            #     print(db_query2)
-            #
-    except mysql.connector.Error as e:
-        print(e)
+        # known_dns = []
+        db_query_select = """
+            SELECT 
+                ipt.id, 
+                dns.dns,
+                ipt.cur_ip_id, 
+                ip1.ip AS current_ip, 
+                ipt.old_ip_id, 
+                ip2.ip AS old_ip 
+            FROM ip_track ipt 
+            LEFT JOIN dns
+                ON dns.id = ipt.dns_id
+            LEFT JOIN ip ip1
+                ON ip1.id = ipt.cur_ip_id 
+            LEFT JOIN ip ip2
+                ON ip2.id = ipt.old_ip_id
+            """
+        with con_db.cursor() as c_select:
+            c_select.execute(db_query_select)
+            result = c_select.fetchall()
+            for row in result:
+                print(row)
+                # if row[1]:
+                #     known_dns.append(row[1])
+        for service in serv_ip:
+            for service_name, service_ip in service.items():
+                with con_db.cursor() as c_select_dns_id:
+                    db_query_select_dns_id = "SELECT id FROM test.dns WHERE dns = '" + service_name + "'"
+                    c_select_dns_id.execute(db_query_select_dns_id)
+                    result = c_select_dns_id.fetchone()
+                    if result is None:
+                        print("Не нашёл в базе dns")
+                        add_dns(service_name, con_db)
+                        c_select_dns_id.execute(db_query_select_dns_id)
+                        result = c_select_dns_id.fetchone()
+                    dns_id = "'" + str(result[0]) + "'"
+                    # print(dns_id)
+                with con_db.cursor() as c_select_ip_id:
+                    db_query_select_ip_id = "SELECT id FROM test.ip WHERE ip = '" + service_ip + "'"
+                    c_select_ip_id.execute(db_query_select_ip_id)
+                    result = c_select_ip_id.fetchone()
+                    if result is None:
+                        print("Не нашёл в базе ip")
+                        add_ip(service_ip, con_db)
+                        c_select_ip_id.execute(db_query_select_ip_id)
+                        result = c_select_ip_id.fetchone()
+                    ip_id = "'" + str(result[0]) + "'"
+                    # print(ip_id)
+                with con_db.cursor() as c_select_track:
+                    db_query_select_track = "SELECT ipt.id FROM ip_track ipt LEFT JOIN dns ON ipt.dns_id = dns.id " \
+                                            "WHERE dns = '" + service_name + "'"
+                    # print(db_query_select_track)
+                    c_select_track.execute(db_query_select_track)
+                    result = c_select_track.fetchone()
+                    if result:
+                        track_id = "'" + str(result[0]) + "'"
+                        # print(track_id)
+                        with con_db.cursor() as c_update_track:
+                            db_query_update_track = "UPDATE ip_track SET cur_ip_id = " + ip_id + ", old_ip_id = " \
+                                                    + ip_id + " WHERE ( id = " + track_id + ")"
+                            # print(db_query_update_track)
+                            c_update_track.execute(db_query_update_track)
+                            con_db.commit()
+                            print(f'Обновил {track_id} {dns_id} {ip_id} {ip_id}')
+                    else:
+                        print("Нет в Списке!")
+                        print("Добавляю!")
+                        with con_db.cursor() as c_insert_new_track:
+                            db_query_insert_new_track = "INSERT INTO ip_track (dns_id, cur_ip_id, old_ip_id) VALUES (" \
+                                                        + dns_id + ", " + ip_id + ", " + ip_id + ")"
+                            # print(db_query_insert_new_track)
+                            c_insert_new_track.execute(db_query_insert_new_track)
+                            con_db.commit()
+                            print(f'Добавил {dns_id} {ip_id} {ip_id}')
+                print()
+    except mysql.connector.Error as err:
+        print(err)
     # time.sleep(100)
     return
 
 
-def print_log(l_ip):
+def add_ip(ip, con_db):
+    try:
+        db_query_insert = "INSERT INTO ip(ip) VALUES ('" + ip + "')"
+        # print(db_query_insert)
+        with con_db.cursor() as c_insert:
+            c_insert.execute(db_query_insert)
+            con_db.commit()
+    except mysql.connector.Error as err:
+        print(err)
+
+
+def add_dns(name, con_db):
+    try:
+        db_query_insert = "INSERT INTO dns(dns) VALUES ('" + name + "')"
+        # print(db_query_insert)
+        with con_db.cursor() as c_insert:
+            c_insert.execute(db_query_insert)
+            con_db.commit()
+    except mysql.connector.Error as err:
+        print(err)
+
+
+def print_log(l_ip, con_db):
     for dict_s in l_ip:
         for service, ip in dict_s.items():
             ip_new = socket.gethostbyname(service)
@@ -195,56 +211,91 @@ def print_log(l_ip):
                     file_err.write(err_string)
                 print(f'[ERROR] {service} IP mismatch: {ip} {ip_new}')
                 dict_s[service] = ip_new
-                save_ip(l_ip)
+                save_change_ip(service, ip_new, con_db)
             print(f'{service} - {ip_new}')
         time.sleep(1)
     print(datetime.now())
 
 
-def ping_ok(sHost):
+def save_change_ip(serv, ip, con_db):
+    with con_db.cursor() as c_select:
+        db_query_select = "SELECT ipt.id, ipt.cur_ip_id FROM ip_track ipt LEFT JOIN dns ON ipt.dns_id = dns.id " \
+                          "WHERE dns = '" + serv + "'"
+        # print(db_query_select)
+        c_select.execute(db_query_select)
+        result = c_select.fetchone()
+        # print(result)
+    with con_db.cursor() as c_select_id_from_ip:
+        db_query_select_id_from_ip = "SELECT id FROM test.ip WHERE ip = '" + ip + "'"
+        # print(db_query_select_id_from_ip)
+        c_select_id_from_ip.execute(db_query_select_id_from_ip)
+        result_ip_from_id = c_select_id_from_ip.fetchone()
+        if result_ip_from_id is None:
+            add_ip(ip, con_db)
+            c_select_id_from_ip.execute(db_query_select_id_from_ip)
+            result_ip_from_id = c_select_id_from_ip.fetchone()
+            # print(result_ip_from_id)
+        ip_id = result_ip_from_id[0]
+        # print(ip_id)
+    with con_db.cursor() as c_update:
+        db_query_update = "UPDATE ip_track SET cur_ip_id = '" + str(ip_id) + "', old_ip_id = '" + str(
+            result[1]) + "' WHERE ( id = '" + str(result[0]) + "')"
+        # print(db_query_update)
+        c_update.execute(db_query_update)
+        con_db.commit()
+
+
+def ping_ok(s_host):
     try:
-        output = subprocess.check_output(
-            "ping -{} 1 {}".format('n' if platform.system().lower() == "windows" else 'c', sHost), shell=True)
-    except Exception as e:
-        print(e)
+        subprocess.check_output(
+            "ping -{} 1 {}".format('n' if platform.system().lower() == "windows" else 'c', s_host), shell=True)
+    except Exception as err:
+        print(err)
         return False
     return True
 
 
-def check_ip(l_ip):
-    dict = {}
+def check_ip(l_ip, file):
+    ip_status = {}
+    dict_out = {}
     for ip in l_ip:
         if ping_ok(ip):
-            dict[ip] = "OK"
-            print(f'{ip} - {dict[ip]}')
+            ip_status[ip] = "OK"
+            print(f'{ip} - {ip_status[ip]}')
         else:
-            dict[ip] = "ALARM!"
-            print(f'{ip} - {dict[ip]}')
-    with open("output/ip.yaml", "w+") as f:
-        yaml.dump(dict, f, indent=2, explicit_start=True, explicit_end=True)
-        yaml.dump(str(datetime.now()), f, explicit_start=True, explicit_end=True)
+            ip_status[ip] = "ALARM!"
+            print(f'{ip} - {ip_status[ip]}')
+    dict_out["date"] = str(datetime.now())
+    dict_out.update(ip_status)
+    write_to_file(dict_out, file)
     return
 
 
-def add_ip_row(list_ip2, con_db):
+def write_to_file(dict_in, file_out):
+    with open(file_out, "w+") as f:
+        yaml.dump(dict_in, f, indent=2, explicit_start=True, explicit_end=True)
+    print()
+
+
+def add_ip_row(list_ip, con_db):
     try:
         known_ip = []
         db_query_select = "SELECT * FROM ip"
         with con_db.cursor() as c_select:
-            print(db_query_select)
+            # print(db_query_select)
             c_select.execute(db_query_select)
             result = c_select.fetchall()
             for row in result:
                 known_ip.append(row[1])
-        for ip in list_ip2:
+        for ip in list_ip:
             if ip not in known_ip:
                 db_query_insert = "INSERT INTO ip(ip) VALUES ('" + ip + "')"
-                print(db_query_insert)
+                # print(db_query_insert)
                 with con_db.cursor() as c_insert:
                     c_insert.execute(db_query_insert)
                     con_db.commit()
-    except mysql.connector.Error as e:
-        print(e)
+    except mysql.connector.Error as err:
+        print(err)
 
 
 def add_dns_row(in_services, con_db):
@@ -252,7 +303,7 @@ def add_dns_row(in_services, con_db):
         known_dns = []
         db_query_select = "SELECT * FROM dns"
         with con_db.cursor() as c_select:
-            print(db_query_select)
+            # print(db_query_select)
             c_select.execute(db_query_select)
             result = c_select.fetchall()
             for row in result:
@@ -260,12 +311,24 @@ def add_dns_row(in_services, con_db):
         for dns in in_services:
             if dns not in known_dns:
                 db_query_insert = "INSERT INTO dns(dns) VALUES ('" + dns + "')"
-                print(db_query_insert)
+                # print(db_query_insert)
                 with con_db.cursor() as c_insert:
                     c_insert.execute(db_query_insert)
                     con_db.commit()
-    except mysql.connector.Error as e:
-        print(e)
+    except mysql.connector.Error as err:
+        print(err)
+
+
+def get_ip(con_db):
+    with con_db.cursor() as c_select:
+        db_query_select = "SELECT ip from ip"
+        c_select.execute(db_query_select)
+        result = c_select.fetchall()
+        if result:
+            list_ip_from_db = []
+            for row in result:
+                list_ip_from_db.append(row[0])
+            return list_ip_from_db
 
 
 if __name__ == '__main__':
@@ -276,10 +339,17 @@ if __name__ == '__main__':
     list_serv_ip = get_one_ip(services)
     conn = connect_db()
     if conn:
+        # add_dns("abc.ru", conn)
+        # add_ip("5.5.5.5", conn)
         add_ip_row(list_input_ip, conn)
         add_dns_row(services, conn)
-        save_ip(list_serv_ip)
-        # while True:
-        #     print_log(list_ip)
-        #     check_ip(list_ip2)
-        close_db(conn)
+        save_init_ip(list_serv_ip, conn)
+        ip_from_db = get_ip(conn)
+        while True:
+            try:
+                print_log(list_serv_ip, conn)
+                check_ip(list_input_ip, "output/ip.yaml")
+                check_ip(ip_from_db, "output/ip_from_db.yaml")
+            except Exception as e:
+                print(e)
+                close_db(conn)
